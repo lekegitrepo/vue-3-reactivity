74f56813a5bb389f3c984dd59453d377d9df81d8
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-05 09:29:58 +0100

    Add effect function and product object

--- depsMap.js
+++ depsMap.js
@@ -7,3 +7,22 @@ function track(key) {
   }
   dep.add(effect);
 }
+
+function trigger(key) {
+  let dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach((effect) => {
+      effect();
+    });
+  }
+}
+
+let product = { price: 5, quantity: 2 };
+let total = 0;
+
+let effect = () => {
+  total = product.price * product.quantity;
+};
+
+track("quantity");
+effect();
