bae54d110d055dbd5a2376895bc46ee173cd78cf
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-05 09:41:20 +0100

    Create track function

---
+++ targetMap.js
@@ -0,0 +1,13 @@
+const targetMap = new WeakMap();
+
+function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()));
+  }
+  dep.add(effect);
+}
