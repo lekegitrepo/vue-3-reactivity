02a61111ff411196084615598be32a873c1948d9
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-12 07:56:47 +0100

    Create reactive function

--- targetMap.js
+++ targetMap.js
@@ -23,6 +23,25 @@ function trigger(target, key) {
   }
 }
 
+function reactive(target) {
+  const handlers = {
+    get(target, key, receiver) {
+      let result = Reflect.get(target, key, receiver);
+      track(target, key); // If this reactive property (target) is GET inside then track the effect to rerun on SET
+      return result;
+    },
+    set(target, key, value, receiver) {
+      let oldValue = target[key];
+      let result = Reflect.set(target, key, value, receiver);
+      if (result && oldValue != value) {
+        trigger(target, key); // If this reactive property (target) has effects to rerun on SET, trigger them.
+      }
+      return result;
+    },
+  };
+  return new Proxy(target, handlers);
+}
+
 let product = { price: 5, quantity: 2 };
 let total = 0;
 
@@ -30,5 +49,5 @@ let effect = () => {
   total = product.price * product.quantity;
 };
 
-track(product, "quantity");
+track(product, 'quantity');
 effect();
